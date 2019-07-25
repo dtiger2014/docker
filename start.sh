44e033d0badf06db074cc8d0987cd66fa8eba237
fanfan <piaolongfan@xiimoon.com>
2019-07-25 18:10:30 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+  
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
